0cb2db1993b984e324742fdaaf24fdbf1977428a
kiayasimone <simone12017@gmail.com>
2023-11-26 14:13:06 -0500

    about page and home page added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=20)
+    description = models.TextField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def finch_list(request):
+    finch_data = [
+        {"name": "Finch1", "color": "Pink", "size": "Big"},
+        {"name": "Finch2", "color": "Orange", "size": "Small"},
+        # Add more finch data dictionaries as needed
+    ]
+
+    return render(request, 'finch_list.html', {'finch_data': finch_data})
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
