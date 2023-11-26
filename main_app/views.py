e49b632332319cdb4208724a12fc12d89d8485e5
kiayasimone <simone12017@gmail.com>
2023-11-26 15:53:23 -0500

    met all user stories and exercises

--- main_app/views.py
+++ main_app/views.py
@@ -3,17 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
-def finch_list(request):
-    finch_data = [
-        {"name": "Finch1", "color": "Pink", "size": "Big"},
-        {"name": "Finch2", "color": "Orange", "size": "Small"},
-        # Add more finch data dictionaries as needed
+finches = [
+    {"name": "Finch1", "color": "Pink", "size": "Big"},
+    {"name": "Finch2", "color": "Orange", "size": "Small"},
+    # Add more finch data dictionaries as needed
     ]
 
-    return render(request, 'finch_list.html', {'finch_data': finch_data})
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches' : finches})
